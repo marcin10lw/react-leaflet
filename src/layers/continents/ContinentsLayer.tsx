2845dcf6cec9a7d7dfde356d45fd90438285a839
marcin10lw <marcin10lw@wp.pl>
2024-01-06 23:08:44 +0100

    refactor(ContinentsLayer): created separate ContinenrGeoJSON component

--- src/layers/continents/ContinentsLayer.tsx
+++ src/layers/continents/ContinentsLayer.tsx
@@ -9,44 +9,46 @@ interface ContinentsLayerProps {
   data: ContinentsCollection;
 }
 
-export const ContinentsLayer = ({ data }: ContinentsLayerProps) => {
-  const continents = data.features as Continent[];
-
+const ContinenrGeoJSON = ({ continent }: { continent: Continent }) => {
   const setGeoFilter = useGeoFilterStore((state) => state.setGeoFilter);
   const geoFilter = useGeoFilterStore((state) => state.geoFilter);
 
+  const continentName = continent.properties.CONTINENT;
+
+  const isSelectedContinent = geoFilter?.selectedContinent === continent;
+
+  const onContinentClick = () => {
+    if (isSelectedContinent) {
+      setGeoFilter(null);
+    } else {
+      setGeoFilter({
+        selectedContinent: continent,
+      });
+    }
+  };
+
+  const pathOptions = isSelectedContinent
+    ? selectedContinentStyles
+    : continentsStyles[continentName];
+
+  return (
+    <GeoJSON
+      key={continentName}
+      data={continent as GeoJSONProps['data']}
+      pathOptions={pathOptions}
+      eventHandlers={{
+        click: onContinentClick,
+      }}
+    >
+      <Tooltip sticky>{continentName}</Tooltip>
+    </GeoJSON>
+  );
+};
+
+export const ContinentsLayer = ({ data }: ContinentsLayerProps) => {
+  const continents = data.features as Continent[];
+
   return continents.map((continent) => {
-    const continentName = continent.properties.CONTINENT;
-
-    const isSelectedContinent = geoFilter?.selectedContinent === continent;
-
-    const onContinentClick = () => {
-      if (isSelectedContinent) {
-        setGeoFilter(null);
-      } else {
-        setGeoFilter({
-          selectedContinent: continent,
-        });
-      }
-    };
-
-    const pathOptions = isSelectedContinent
-      ? selectedContinentStyles
-      : continentsStyles[continentName];
-
-    return (
-      <GeoJSON
-        key={continentName}
-        data={continent as GeoJSONProps['data']}
-        pathOptions={pathOptions}
-        eventHandlers={{
-          click: onContinentClick,
-        }}
-      >
-        <Tooltip direction="top" sticky offset={[0, -10]}>
-          {continentName}
-        </Tooltip>
-      </GeoJSON>
-    );
+    return <ContinenrGeoJSON continent={continent} />;
   });
 };
