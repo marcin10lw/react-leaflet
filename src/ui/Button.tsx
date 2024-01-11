d985e12e5e9e0b8855a24dbe2b1c7cbd0ddba7fc
marcin10lw <marcin10lw@wp.pl>
2024-01-11 23:22:13 +0100

    feat(Button): created reusable button

---
+++ src/ui/Button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, forwardRef } from 'react';
+
+import { VariantProps, cva } from 'class-variance-authority';
+import { cn } from 'src/utils';
+
+export const buttonVariants = cva('shadow flex items-center justify-center', {
+  variants: {
+    variant: {
+      default: 'bg-slate-600 text-white hover:scale-95',
+    },
+    size: {
+      default: 'w-[40px] h-[40px]',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+});
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, size, variant, ...props }, ref) => {
+    return (
+      <button ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props} />
+    );
+  },
+);
