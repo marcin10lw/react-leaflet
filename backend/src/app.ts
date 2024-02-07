35716be8505f6ac157b8529e687c281379d0a746
marcin10lw <marcin10lw@wp.pl>
2024-02-07 22:49:36 +0100

    feat(express_app): setup body parser, morgan and cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,21 @@
 require("dotenv").config();
 import express from "express";
 import config from "config";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: config.get<string>("origin"),
+    methods: "GET",
+  })
+);
 
 const port = config.get<number>("port");
 
